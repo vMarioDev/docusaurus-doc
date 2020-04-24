e36e685810d0c1d78cfa4907299dc481c97ca4c6
Jasinski <mariusz.jasinski@atos.net>
2020-04-24 14:43:41 +0100

    change comment

--- js/codetabs.js
+++ js/codetabs.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-// Turn off ESLint for this file because it's sent down to users as-is.
+// Turn off ESLint for this file because it's sent down to users as-is.bbg
 
 /* eslint-disable */
 window.addEventListener('load', function() {
